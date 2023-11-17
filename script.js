a65d7d3ba47f020b594770fc4cec3142b2bc919d
Jagmohan-cmd <141887538+Jagmohan-cmd@users.noreply.github.com>
2023-11-17 09:22:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let counter = document.getElementById("counter");
+let btn = document.getElementById("incrementBtn");
+let counterValue = 0;
+btn.addEventListener("click" , myFunction);
+
+
+function myFunction(){
+	alert(`Un-incremented value: ${counterValue}`);
+	counterValue++;
+	counter.innerText = counterValue;
+}
